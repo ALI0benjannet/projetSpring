3d0be49f9bef0d600433840b68d8773cbc1b3abe
ALI0benjannet <alialilou54098546@gmail.com>
2025-09-29 14:34:32 +0100

    jajout les getters et les setters

--- src/main/java/org/esprim/gestionfoyer/entity/Chambre.java
+++ src/main/java/org/esprim/gestionfoyer/entity/Chambre.java
@@ -1,10 +1,18 @@
 package org.esprim.gestionfoyer.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.annotation.processing.Generated;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Chambre {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
