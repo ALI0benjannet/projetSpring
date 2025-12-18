ba3a37590952afc69c288bd6b5be10903eb3833a
ALI0benjannet <alialilou54098546@gmail.com>
2025-12-18 15:22:37 +0100

    crier l'AspectProcess (Around)

--- src/main/java/org/esprim/gestionfoyer/config/AspectProcess.java
+++ src/main/java/org/esprim/gestionfoyer/config/AspectProcess.java
@@ -2,7 +2,9 @@ package org.esprim.gestionfoyer.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -11,14 +13,29 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Slf4j
 public class AspectProcess {
-@Before("execution(* org.esprim.gestionfoyer.services.*.*(..))")
+    @Before("execution(* org.esprim.gestionfoyer.services.*.*(..))")
     public void logMothodEntry (JoinPoint joinPoint) {
         String name = joinPoint.getSignature().getName();
         log.info("***** In method " + name + " : ");
-}
+    }
+
     @After("execution(* org.esprim.gestionfoyer.services.*.*(..))")
     public void logMothodExit (JoinPoint joinPoint) {
         String name = joinPoint.getSignature().getName();
         log.info("***** Exiting method " + name + " : ");
     }
+
+    @Around("execution(* org.esprim.gestionfoyer.services.*.*(..))")
+    public Object profile(ProceedingJoinPoint pjp) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        try {
+            Object obj = pjp.proceed();
+            return obj;
+        }finally {
+            long endTime = System.currentTimeMillis()-startTime;
+            String methodName = pjp.getSignature().getName();
+            log.info("***** Method " + methodName + " : " + endTime + " ms");
+        }
+    }
+
 }
