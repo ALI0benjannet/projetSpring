a29bab9639b8b6731d0d9aa89b61744e0f9c042d
ALI0benjannet <alialilou54098546@gmail.com>
2025-12-18 14:19:33 +0100

    crier l'AspectProcess

--- src/main/java/org/esprim/gestionfoyer/GestionFoyerApplication.java
+++ src/main/java/org/esprim/gestionfoyer/GestionFoyerApplication.java
@@ -2,8 +2,9 @@ package org.esprim.gestionfoyer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+@EnableAspectJAutoProxy
 @EnableScheduling
 @SpringBootApplication
 public class GestionFoyerApplication {
